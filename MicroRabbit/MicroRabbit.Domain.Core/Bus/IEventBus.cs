7db355c0caeb929f1dadcb21e2cd136e40d6dcef
Payam Ardeshirzad <payam@ardeshirzad.com>
2020-04-27 12:34:58 +1200

    Implemented Interfaces and Abstract Classes for Domain.Core

---
+++ MicroRabbit/MicroRabbit.Domain.Core/Bus/IEventBus.cs
@@ -0,0 +1,24 @@
+﻿using MicroRabbit.Domain.Core.Commands;
+using MicroRabbit.Domain.Core.Events;
+using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MicroRabbit.Domain.Core.Bus
+{
+    public interface IEventBus
+    {
+        // Send Commands to MediatR
+        Task SendCommand<T>(T command) where T : Command;
+
+        // Publish an event. as event is reserve keyword @event is declared
+        void Publish<T>(T @event) where T : Event;
+
+        // Subscribe to published events. EventType and EventHandler
+        void Subscribe<T, TH>()
+            where T : Event
+            where TH : IEventHandler<T>;
+
+    }
+}
---
+++ MicroRabbit/MicroRabbit.Domain.Core/Events/Message.cs
@@ -0,0 +1,19 @@
+﻿using MediatR;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace MicroRabbit.Domain.Core.Events
+{
+    // Any request to MediateR expects a boolean value back
+    public abstract class Message: IRequest<bool>
+    {
+        public string MessageType { get; protected set; }
+
+        protected Message()
+        {
+            // using reflection to get the type name
+            MessageType = GetType().Name;
+        }
+    }
+}
